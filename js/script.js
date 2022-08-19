9c432727125c9fd8b261042882973e40f6be08dc
James 'Dante' Midzi <jamesmidzi@gmail.com>
2022-08-19 10:00:37 +0200

    everything is working fine, except for the classes on the save button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,66 @@
 const form = document.querySelector("#generate-form");
 const qr = document.querySelector("#qrcode");
 
+// Submit Button
 const onGenerateQR = (event) => {
   event.preventDefault();
+
+  clearUI();
+
+  const url = document.querySelector("#url").value;
+  const size = document.querySelector("#size").value;
+
+  // validate url
+  if (url === "") {
+    alert("Please enter a valid URL");
+  } else {
+    showSpinner();
+
+    setTimeout(() => {
+      hideSpinner();
+
+      generateQRCode(url, size);
+
+      setTimeout(() => {
+        const saveUrl = qr.querySelector("img").src;
+        createSaveBtn(saveUrl);
+      }, 50);
+    }, 1000);
+  }
 };
+
+const generateQRCode = (url, size) => {
+  const qrcode = new QRCode("qrcode", {
+    text: url,
+    width: size,
+    height: size,
+  });
+};
+
+const showSpinner = () => {
+  document.querySelector("#spinner").style.display = "block";
+};
+const hideSpinner = () => {
+  document.querySelector("#spinner").style.display = "none";
+};
+
+// Clear the code
+const clearUI = () => {
+  qr.innerHTML = "";
+};
+
+// create save button
+const createSaveBtn = (saveUrl) => {
+  const link = document.createElement("a");
+  link.id = "save";
+  link.classList =
+    "bg-blue-700 hover:bg:blue-600 text-gray-100 font-bold py-2 rounded w-1/3 m-auto my-5";
+  link.href = saveUrl;
+  link.download = "qrcode";
+  link.innerHTML = "Save Image";
+  document.querySelector("#generated").appendChild(link);
+};
+
+hideSpinner();
 
 form.addEventListener("submit", onGenerateQR);
